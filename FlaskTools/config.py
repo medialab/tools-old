b4bd5a9d2055a08f5ac82c36e4d8652d1d39e5d0
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2019-07-08 16:09:56 +0200

    reorganize files

--- FlaskTools/config.py
+++
@@ -1,9 +0,0 @@
-from os import path
-
-HOST = "0.0.0.0"
-PORT = 5000
-DATA_FOLDER = "/datas/www"
-SOURCE_FOLDER = path.join(DATA_FOLDER, "source")
-TEMPLATE_FOLDER = path.join(SOURCE_FOLDER, "templates")
-DEBUG = True
-
